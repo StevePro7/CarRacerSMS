19de64aedbeadad90ec7fb8335a4a1623144c382
StevePro7 <steveprozune@hotmail.com>
2020-08-29 18:24:54 +0100

    no message

--- main.asm
+++ main.asm
@@ -1,4 +1,258 @@
+; -------------------------------------------------------------;
+;                      RACER                                   ;
+; -------------------------------------------------------------;
 
+.sdsctag 1.12, "Racer", "Speed and/or die!", "Anders S. Jensen"
+
+.memorymap                 ; create 2 x 16 kb slots for rom.
+       defaultslot 0
+       slotsize $4000
+       slot 0 $0000        ; rom bank 0 (0-16 kb).
+       slot 1 $4000        ; rom bank 1 (16-32 kb).
+       slotsize $2000
+       slot 2 $c000        ; ram.
+.endme
+
+.rombankmap                ; map rom to 2 x 16 kb banks.
+       bankstotal 2
+       banksize $4000
+       banks 2
+.endro
+
+.equ   vspeed 7            ; players' vertical speed.
+.equ   hspeed 3            ; horiz. speed of player and enemy.
+.equ   espeed 4            ; enemy's vertical speed.
+.equ   rightb $8c          ; right border of the road.
+.equ   leftb $14           ; left border of the road.
+
+; Map of the sprite attribute table (sat) buffer.
+; Contains sprites' vertical position (vpos), horizontal posi-
+; tion (hpos) and character codes (cc).
+
+.equ highvp $c000          ; first hiscore vpos.
+.equ highhp $c080          ; first highscore hpos.
+.equ highcc $c081          ; first hiscore cc.
+
+.equ scorvp $c004          ; first score vpos.
+.equ scorhp $c088          ; first score hpos.
+.equ scorcc $c089          ; first score cc.
+
+.equ plrvp $c008           ; first player vpos.
+.equ plrhp $c090           ; first player hpos.
+.equ plrcc $c091           ; first player cc.
+
+.equ maevp $c018           ; first Mae vpos.
+.equ maehp $c0b0           ; first Mae hpos.
+.equ maecc $c0b1           ; first Mae cc.
+
+.equ ashvp $c028           ; first Ash vpos.
+.equ ashhp $c0d0           ; first Ash hpos.
+.equ ashcc $c0d1           ; first Ash cc.
+
+.equ endspr $c038          ; first unused sprite.
+
+ ; Organize ram - create the sat buffer and variables.
+
+.enum $c000 export         ; export labels to symbol file.
+
+       satbuf dsb 256      ; sprite attribute table buffer.
+                           ; see the map for object offsets.
+
+       scroll db           ; vdp scroll register buffer.
+       input db            ; input from player 1 controller.
+       iflag db            ; frame interrupt flag.
+       frame db            ; frame counter.
+       status db           ; vdp status (for collision detect.).
+       hscore dsb 4        ; 4 digits for hiscore 0000-9999.
+       score dsb 4         ; 4 digits for score 0000-9999.
+       record db           ; new record = hiscore is beaten!
+
+       ashdir db           ; Ash's direction.
+       ashy db             ; Ash y (vertical position).
+       ashx db             ; Ash x (horizontal position).
+
+       maedir db           ; Mae's direction.
+       maey db             ; Mae y (vertical position).
+       maex db             ; Mae x (horizontal position).
+       maedel db           ; Mae delay.
+
+       ply db              ; Player y.
+       plx db              ; Player x.
+.ende
+
+.bank 0 slot 0
+.org 0
+       di                  ; disable interrupts.
+       im 1                ; interrupt mode 1.
+       ld sp,$dff0         ; default stack pointer address.
+       jp inigam           ; initialize game.
+
+; Read the vdp status flag at every frame interrupt.
+; Sprite collision is read from bit 5 of the status flag.
+; Set the frame interrupt flag.
+
+.orga $0038                ; frame interrupt address. 
+        ex af,af'           ; save accumulator in its shadow reg. 
+        in a,$bf            ; get vdp status / satisfy interrupt. 
+        ld (status),a       ; save vdp status in ram. 
+        ld a,1              ; load accumulator with raised flag. 
+        ld (iflag),a        ; set interrupt flag. 
+        ex af,af'           ; restore accumulator. 
+        ei                  ; enable interrupts.
+        reti                ; return from interrupt. 
+
+; Disable the pause button - this is an unforgiving game!
+
+.orga $0066                ; pause button interrupt.
+       retn                ; disable pause button.
+
+; Initialize game.
+; Overwrite the first 4 kb of ram with zeroes.
+
+inigam ld hl,$c000         ; point to beginning of ram.
+       ld bc,$1000         ; 4 kb to fill.
+       ld a,0              ; with value 0.
+       call mfill          ; do it!
+
+; Use the initialized ram to clean all of vram.
+
+       ld hl,$0000         ; prepare vram for data at $0000.
+       call vrampr
+       ld b,4              ; write 4 x 4 kb = 16 kb.
+-      push bc             ; save the counter.
+       ld hl,$c000         ; source = freshly initialized ram.
+       ld bc,$1000         ; 4 kb of zeroes.
+       call vramwr         ; purge vram.
+       pop bc              ; retrieve counter.
+       djnz -
+
+ ; Initialize PSGLib.
+
+       call PSGInit        ; credit goes to sverx!
+
+; Load various assets into vram.
+
+       ld hl,$c010         ; color bank 2, color 0 (sprites).
+       call vrampr         ; prepare vram.
+       ld hl,palspr        ; sprite palette data.
+       ld bc,5             ; 5 colors.
+       call vramwr         ; set sprite palette.
+
+       ld hl,$2600         ; first tile @ index $130.
+       call  vrampr        ; prepare vram at the above address.
+       ld hl,blchar        ; the blue characters - for hiscore.
+       ld bc,10*32         ; 10 tiles, each tile is 32 bytes.
+       call vramwr         ; write blue character tils to vram.
+
+       ld hl,$2800         ; first tile @ index $140.
+       call  vrampr        ; prepare vram at the above address.
+       ld hl,rdchar        ; the red characters - for score.
+       ld bc,10*32         ; 10 tiles, each tile is 32 bytes.
+       call vramwr         ; write red characters to vram.
+
+       ld hl,$2000         ; first tile @ index 256.
+       call vrampr         ; prepare vram.
+       ld hl,pltile        ; player car tile data.
+       ld bc,16*32         ; 16 tiles, 32 bytes each.
+       call vramwr         ; write player car tiles to vram.
+
+       ld hl,$2200         ; first tile @ index 272.
+       call vrampr         ; prepare vram.
+       ld hl,entile        ; enemy car tile data.
+       ld bc,16*32         ; 16 tiles, 32 bytes each.
+       call vramwr         ; write enemy car tiles to vram.
+
+       ld hl,$2400         ; first tile @ index 288.
+       call vrampr         ; prepare vram.
+       ld hl,plcras        ; crashed player car data.
+       ld bc,16*32         ; 16 tiles, 32 bytes each.
+       call vramwr         ; write crashed player tiles to vram.
+
+       ld hl,$c000         ; color bank 1, color 0.
+       call vrampr         ; prepare vram.
+       ld hl,bgpal         ; background palette.
+       ld bc,4             ; 4 colors.
+       call vramwr         ; set background palette.
+
+; Initialize hiscore.
+
+       ld hl,hscore+1      ; point to hundreds-digit in hiscore.
+       ld b,2              ; we want to add 2 to this digit.
+       call addsco         ; initialize hiscore counter to 0200.
+
+; Initialize variables - once per game.
+
+       xor a
+       ld (maedir),a       ; Mae goes left.
+       ld a,20             ;
+       ld (ashx),a         ; set Ash x.
+       ld a,100            ;
+       ld (maex),a         ; set Mae x.
+       ld a,140            ;
+       ld (ply),a          ; set player y.
+
+; Prepare for the title screen loop.
+
+prepti
+
+; Initialize the VDP registers.
+
+       ld hl,regdat        ; point to register init data.
+       ld b,11             ; 11 bytes of register data.
+       ld c,$80            ; VDP register command byte.
+
+-:     ld a,(hl)           ; load one byte of data into A.
+       out ($bf),a         ; output data to VDP command port.
+       ld a,c              ; load the command byte.
+       out ($bf),a         ; output it to the VDP command port.
+       inc hl              ; inc. pointer to next byte of data.
+       inc c               ; inc. command byte to next register.
+       djnz -              ; jump back to '-' if b > 0.
+
+; Load title screen specific assets into vram.
+
+       ld hl,$0000         ; first tile @ index 0.
+       call vrampr         ; prepare vram.
+       ld hl,titile        ; title screen tile data.
+       ld bc,77*32         ; 77 tiles, each tiles is 32 bytes.
+       call vramwr         ; write title screen tiles to vram.
+
+       ld hl,$3800         ; point to name table.
+       call vrampr         ; prepare vram.
+       ld hl,timap         ; title screen tile map data.
+       ld bc,32*24*2       ; 32 x 24 tiles, each is 2 bytes.
+       call vramwr         ; write name table to vram.
+
+; Set the hiscore y and x position.
+
+       ld hl,tshigh        ; point to hiscore init data for title screen.
+       ld de,highvp        ; target the (hi-) score sat buffer.
+       ld bc,5             ; write 5 vpos bytes (incl. $d0).
+       ldir                ; do it.
+       ld hl,tshigh+5      ; point to hiscore init data.
+       ld de,highhp        ; target buffer again.
+       ld bc,8             ; write 8 hpos and cc bytes.
+       ldir
+
+; Create sprite data in the sat buffer for the hiscore sprites,
+; and load the updated buffer into vram (screen is off now).
+
+       call upbuf          ; update sat buffer.
+       call ldsat          ; load sat in vram with buffer.
+
+; Turn screen on.
+
+       xor a
+       ld b,9              ; reset scroll value.
+       call setreg         ; set register 1.
+
+       ld a,%11100001      ; enable screen + zoomed sprites.
+       ld b,1              ; register 1.
+       call setreg         ; set register.
+
+       ei                  ; turn interrupts on.
+       
+       
 
 loop:
     jp loop
