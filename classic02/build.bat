d9c3b9af1f795d0cb048536f9d2da7f3a3b943fe
StevePro7 <steveprozune@hotmail.com>
2020-12-10 20:44:24 +0000

    no message

--- classic02/build.bat
+++ classic02/build.bat
@@ -1,11 +1,16 @@
 cls
 
+if exist test.o del test.o
+
 wla-z80 -o test.o main.asm 
 
 echo [objects] > linkfile
 echo test.o >> linkfile
 
 wlalink linkfile output.sms
+
+if exist output.sms.sym del output.sms.sym
+if exist test.o del test.o
 
 ::java -jar C:\SEGA\Emulicious\Emulicious.jar output.sms
 output.sms
